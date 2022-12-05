5f9427f53086004f7b5a3687f1c8f5338a4a7a04
samuel1109123 <78217677+samuel1109123@users.noreply.github.com>
2022-12-05 12:43:41 -0500

    fixed project

--- Ecommerce.Api.Search/Services/CustomersService.cs
+++ Ecommerce.Api.Search/Services/CustomersService.cs
@@ -24,13 +24,13 @@ namespace Ecommerce.Api.Search.Services
         {
             try
             {
-                var client = httpClientFactory.CreateClient("CustomersServuce");
+                var client = httpClientFactory.CreateClient("CustomersService");
                 var response = await client.GetAsync($"api/customers/{id}");
                 if (response.IsSuccessStatusCode)
                 {
                     var content = await response.Content.ReadAsByteArrayAsync();
                     var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
-                    var result = JsonSerializer.Deserialize<IEnumerable<Order>>(content, options);
+                    var result = JsonSerializer.Deserialize<dynamic>(content, options);
                     return (true, result, null);
                 }
                 return (false, null, response.ReasonPhrase);
